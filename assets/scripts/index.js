a119821c0c71d6d5ce4d38c4e0012c3370ef6a9e
msyao-github <msyaois@gmail.com>
2016-02-28 14:55:00 -0500

    Edited files for login credentials

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,34 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+require('./api-auth'); //login credentials
+
+
+
+let getArticles = function(){
+ $.ajax({
+   url: "http://localhost:3000/articles",
+   method: 'GET',
+   dataType: 'json'
+ }).done(function(articles){
+  //  displayBooks(articles);
+   console.log(articles);
+ });
+};
+
+// let displayArticles = function(articles){
+//  let articleListingTemplate = require('./article-listing.handlebars');
+//  $('.content').append(articleListingTemplate({articles}));
+//    getUsers();
+//  });
+//
+// };
+
+
+let clear = function(){
+ $('.content').empty();
+}
+
+$(document).ready(function(){
+// getArticles();
+});
