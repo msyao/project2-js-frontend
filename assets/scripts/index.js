c040b8a5fc1ffc7911f194ccb7abfe04d041215d
msyao-github <msyaois@gmail.com>
2016-05-04 20:18:13 -0400

    embed twitter widget

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -41,6 +41,8 @@ $('#edit-article').on('submit', function(e) {
   uiDisplay.hideModal();
 });
 
+// twitter embed
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
 $(document).ready(function() {
   $('.articles-tab').on('click', function() {
